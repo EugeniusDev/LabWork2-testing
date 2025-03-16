88bfd9408ecede776174159dd511b249de776dd8
EugeniusDev <yevhenii.froliak@gmail.com>
2025-03-16 15:25:53 +0200

    Initial commit

---
+++ src/main/java/edu/froliak/Ellipse.java
@@ -0,0 +1,10 @@
+package edu.froliak;
+
+/*
+  @author eugen
+  @project LabWork2
+  @class Ellipse
+  @version 1.0.0
+  @since 3/16/2025 - 15.24
+*/public class Ellipse {
+}
---
+++ src/main/java/edu/froliak/Main.java
@@ -0,0 +1,13 @@
+package edu.froliak;
+
+/*
+  @author eugen
+  @project Default (Template) Project
+  @class ${NAME}
+  @version 1.0.0
+  @since 3/16/2025 - 15.15
+*/public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
